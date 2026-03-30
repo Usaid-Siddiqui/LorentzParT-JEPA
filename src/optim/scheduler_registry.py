6b2651c643d12120de27b8ba6ae760778e267ae9
Usaid Siddiqui <usiddiqui@crimson.ua.edu>
2026-03-30 01:18:08 -0500

    added scheduler

--- src/optim/scheduler_registry.py
+++ src/optim/scheduler_registry.py
@@ -4,5 +4,6 @@ from torch.optim import lr_scheduler
 SCHEDULER_REGISTRY = {
     'exponential_lr': lr_scheduler.ExponentialLR,
     'reduce_lr_on_plateau': lr_scheduler.ReduceLROnPlateau,
-    # Add more schedulers here as needed
+    'cosine_annealing_lr': lr_scheduler.CosineAnnealingLR,
+    'step_lr': lr_scheduler.StepLR,
 }
